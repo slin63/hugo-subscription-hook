2ed5ab7dca2f6bbfef3ec57512c8871da7b012b1
Shean Lin <shean.lin2018@gmail.com>
2020-03-28 01:47:23 -0500

    add methods for writing and keeping track of post#

--- website.py
+++ website.py
@@ -1,11 +1,30 @@
 # Checks if the website has new posts.
 import requests
+import pathlib
 
 
+script_path = str(pathlib.Path(__file__).parent.absolute())
+post_count = script_path + "/post_count.txt"
 # WEBSITE = "https://www.chronicpizza.net/"
 WEBSITE = "http://localhost:1313/"
 # The title of every post contains one of this character.
 # We can easily use it to count how many posts there are.
 SPECIAL_CHAR = "•"
 
-def count_counters
+def check_post_count():
+    r = requests.get(WEBSITE)
+    return r.text.count(SPECIAL_CHAR)
+
+def get_post_count():
+    with open(post_count) as file:
+        s = file.read()
+    return int(s)
+
+def write_post_count(c):
+     with open(post_count, "w") as file:
+        file.write(str(c))
+
+x = check_post_count()
+z = get_post_count()
+print(z)
+write_post_count(x)
